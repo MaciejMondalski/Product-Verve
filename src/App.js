43d2bdf5a1e1460bd78575c68d8c69c6b3a6ac3c
MaciejMondalski <maciej.mondalski@gmail.com>
2022-03-26 23:31:20 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import './App.css';
+
+// pages and components
+import Dashboard from './pages/Dashboard';
+import Create from './pages/Create';
+import Login from './pages/Login';
+import Project from './pages/Project';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='projects/:id' element={<Project />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
