9a90150dbbcb440276391c0e4cb585839cf68a04
MaciejMondalski <maciej.mondalski@gmail.com>
2022-04-03 19:17:56 +0200

    created signup form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,57 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+
 function Signup() {
-  return <div>Signup</div>;
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
+
+  return (
+    <StyledAuthForm>
+      <h2>Sign up</h2>
+      <label>
+        <span>name:</span>
+        <input
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>profile picture:</span>
+        <input required type='file' />
+      </label>
+      <button className='btn'>Signup</button>
+    </StyledAuthForm>
+  );
 }
+
+const StyledAuthForm = styled.form`
+  max-width: 360px;
+  margin: 60px auto;
+  padding: 40px;
+  border: 1px solid #ddd;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.05);
+  background: #fff;
+`;
 
 export default Signup;
