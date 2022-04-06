835f37b931f8d55dec81131535d82ae0910a1295
MaciejMondalski <maciej.mondalski@gmail.com>
2022-04-06 23:15:37 +0200

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import styled from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 // pages and components
