a64b9a3fb0692e335a6c0c0e2f4acf867b009af4
MaciejMondalski <maciej.mondalski@gmail.com>
2022-04-16 23:45:05 +0200

    .

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,52 @@
+import styled from 'styled-components';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <StyledLogin onSubmit={handleSubmit}>
+      <h2>Sign up</h2>
+      <label>
+        <span>display name:</span>
+        <input
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>profile picture:</span>
+        <input required type='file' onChange={handleFileChange} />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
+      </label>
+      {!isPending && <button className='btn'>Sign up</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          loading
+        </button>
+      )}
+
+      {error && <div className='error'>{error}</div>}
+    </StyledLogin>
+  );
 }
+
+const StyledLogin = styled.div``;
 
 export default Login;
