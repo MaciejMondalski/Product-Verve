4d3bd085342ac6e355c0f362ff4ab01577d74a40
MaciejMondalski <maciej.mondalski@gmail.com>
2022-04-17 23:43:13 +0200

    change online status

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { projectAuth } from '../firebase/config';
+import { projectAuth, projectFirestore } from '../firebase/config';
 import { useAuthContext } from '../hooks/useAuthContext';
 
 export const useLogin = () => {
@@ -15,6 +15,12 @@ export const useLogin = () => {
     try {
       // login user
       const res = await projectAuth.signInWithEmailAndPassword(email, password);
+
+      // update online status
+      await projectFirestore
+        .collection('users')
+        .doc(res.user.uid)
+        .update({ online: true });
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user });
