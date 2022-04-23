9a1513b7ffa430fd085d93361b41fd4b60b5bb8b
MaciejMondalski <maciej.mondalski@gmail.com>
2022-04-23 21:42:05 +0200

    created the Avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+function Avatar({ src }) {
+  return (
+    <StyledAvatar>
+      <img src={src} alt='user avatar' />
+    </StyledAvatar>
+  );
+}
+
+const StyledAvatar = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default Avatar;
