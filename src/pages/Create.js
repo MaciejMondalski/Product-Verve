ad99f5308460fcfbce04692ea33587562622c6de
MaciejMondalski <maciej.mondalski@gmail.com>
2022-04-29 23:01:03 +0200

    create project form

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,68 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+
 function Create() {
-  return <div>Create</div>;
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [category, setCategory] = useState('');
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate);
+  };
+
+  return (
+    <StyledCreate>
+      <h2 className='page-title'>Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type='text'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project Details:</span>
+          <textarea
+            required
+            type='text'
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type='date'
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          {/* category select here */}
+        </label>
+        <label>
+          <span>Assign to:</span>
+          {/* assignee select here */}
+        </label>
+        <button className='btn'>Add Project</button>
+      </form>
+    </StyledCreate>
+  );
 }
+
+const StyledCreate = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  width: 100%;
+`;
 
 export default Create;
