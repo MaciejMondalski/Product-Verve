b42dacc82370f98a06074bdd2a3ba711d7250ed6
MaciejMondalski <maciej.mondalski@gmail.com>
2022-05-08 21:31:06 +0200

    Submit new project info

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import Select from 'react-select';
 import { useCollection } from '../hooks/useCollection';
+import { timestamp } from '../firebase/config';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const categories = [
   { value: 'development', label: 'Development' },
@@ -13,6 +15,7 @@ const categories = [
 function Create() {
   const { documents } = useCollection('users');
   const [users, setUsers] = useState([]);
+  const { user } = useAuthContext();
 
   // form field values
   const [name, setName] = useState('');
@@ -43,7 +46,31 @@ function Create() {
       return;
     }
 
-    console.log(name, details, dueDate, category.value, assignedUsers);
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid,
+    };
+
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id,
+      };
+    });
+
+    const project = {
+      name,
+      details,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy,
+      assignedUsersList,
+    };
+
+    console.log(project);
   };
 
   return (
