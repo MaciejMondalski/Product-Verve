d57646a860ac85fe136e39e6b81a9bf0ad1d34c2
MaciejMondalski <maciej.mondalski@gmail.com>
2022-05-13 21:57:04 +0200

    display project in the dashboard

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,71 @@
-import styles from 'styled-components';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Avatar from './Avatar';
 
 function ProjectList({ projects }) {
   return (
-    <div>
-      {projects.length === 0 && <p>There are no projects to show</p>}
+    <StyledProjectList>
+      {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className='assigned-to'>
+            <p>
+              <strong>Assigned to:</strong>
+            </p>
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
-    </div>
+    </StyledProjectList>
   );
 }
+
+const StyledProjectList = styled.div`
+  margin-top: 40px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-gap: 20px;
+
+  a {
+    background-color: #fff;
+    padding: 16px;
+    border-radius: 6px;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.05);
+    text-decoration: none;
+    color: inherit;
+  }
+  h4 {
+    font-size: 0.9em;
+    color: var(--heading-color);
+  }
+  p {
+    color: var(--text-color);
+    font-size: 0.9em;
+  }
+  .assigned-to {
+    margin-top: 20px;
+    padding-top: 10px;
+    border-top: 1px solid #eee;
+  }
+  ul {
+    margin: 10px 0;
+    display: flex;
+  }
+  li {
+    margin-right: 10px;
+  }
+  .avatar {
+    width: 30px;
+    height: 30px;
+  }
+`;
 
 export default ProjectList;
