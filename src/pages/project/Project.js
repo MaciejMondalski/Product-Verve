b1de5310710cb13135bcaccccb81f23172f0a259
MaciejMondalski <maciej.mondalski@gmail.com>
2022-05-22 22:35:03 +0200

    created Project component

---
+++ src/pages/project/Project.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import { useDocument } from '../../hooks/useDocument';
+import ProjectSummary from './ProjectSummary';
+import ProjectComments from './ProjectComments';
+
+function Project() {
+  const { id } = useParams();
+  const { error, document } = useDocument('projects', id);
+
+  if (error) {
+    return <div className='error'>{error}</div>;
+  }
+
+  console.log(error);
+  if (!document) {
+    return <div className='loading'>Loading...</div>;
+  }
+  return (
+    <StyledProject>
+      <ProjectSummary project={document} />
+      <ProjectComments />
+    </StyledProject>
+  );
+}
+
+const StyledProject = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  align-items: start;
+  grid-gap: 60px;
+  width: 100%;
+`;
+
+export default Project;
