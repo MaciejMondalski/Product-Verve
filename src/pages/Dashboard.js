7593e5663c279998becee6caa9cc9753f0cb3d2d
MaciejMondalski <maciej.mondalski@gmail.com>
2022-05-22 22:34:37 +0200

    .

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,17 @@
 import ProjectList from '../components/ProjectList';
 import { useCollection } from '../hooks/useCollection';
+import styled from 'styled-components';
 
 function Dashboard() {
   const { documents, error } = useCollection('projects');
   return (
-    <div>
+    <StyledDashboard>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
       {documents && <ProjectList projects={documents} />}
-    </div>
+    </StyledDashboard>
   );
 }
 
+const StyledDashboard = styled.div``;
 export default Dashboard;
