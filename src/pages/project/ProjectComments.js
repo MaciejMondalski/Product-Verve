b5d369db8ce83a3ca92eb00367c05a6572927aef
MaciejMondalski <maciej.mondalski@gmail.com>
2022-05-28 23:54:34 +0200

    comments styling

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,6 +3,7 @@ import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import styled from 'styled-components';
 import { useFirestore } from '../../hooks/useFirestore';
+import Avatar from '../../components/Avatar';
 
 const ProjectComments = ({ project }) => {
   const [newComment, setNewComment] = useState('');
@@ -30,6 +31,27 @@ const ProjectComments = ({ project }) => {
   return (
     <StyledComments>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className='top-row'>
+                <div className='comment-author'>
+                  <Avatar src={comment.photoURL} />
+                  <p>{comment.displayName}</p>
+                </div>
+                <div className='comment-date'>
+                  <p>date here</p>
+                </div>
+              </div>
+              <div className='comment-content'>
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+
       <form className='add-comment' onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
@@ -52,6 +74,42 @@ const StyledComments = styled.div`
   textarea {
     min-height: 40px;
     font-size: 1.5em;
+  }
+
+  /* project comments */
+
+  h4 {
+    color: var(--heading-color);
+  }
+
+  li {
+    padding: 16px;
+    border-radius: 4px;
+    border: 1px solid #f2f2f2;
+    margin-top: 20px;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.05);
+    background: #fff;
+  }
+
+  .comment-author {
+    display: flex;
+    align-items: center;
+    color: var(--title-color);
+
+    .avatar {
+      width: 30px;
+      height: 30px;
+      margin-right: 10px;
+    }
+  }
+  .comment-date {
+    color: var(--text-color);
+    font-size: 0.9em;
+    margin: 4px 0 10px;
+  }
+  .comment-content {
+    color: var(--text-color);
+    font-size: 0.9em;
   }
 `;
 
