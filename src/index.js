9f101f9ec3eb3c6726e687403e6b635c13dfbda5
MaciejMondalski <maciej.mondalski@gmail.com>
2022-07-09 16:04:59 +0200

    wrapped app in pagination context provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import { DashboardContextProvider } from './context/DashboardContext';
+import { PaginationContextProvider } from './context/PaginationContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
       <DashboardContextProvider>
-        <App />
+        <PaginationContextProvider>
+          <App />
+        </PaginationContextProvider>
       </DashboardContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
