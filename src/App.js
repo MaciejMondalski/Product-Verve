a9294a7997720617ee9e7740fdb1e249db420906
MaciejMondalski <maciej.mondalski@gmail.com>
2022-07-10 19:43:04 +0200

    adjusted routes to pagination

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import UserList from './components/UserList';
 import { useState } from 'react';
+import { usePaginationContext } from './hooks/usePaginationContext';
 
 function App() {
   const { user, authIsReady } = useAuthContext();
   const [createModal, setCreateModal] = useState(false);
+  const { currentPage, setCurrentPage } = usePaginationContext();
 
   return (
     <div className='App'>
@@ -28,9 +30,16 @@ function App() {
             <div className={user ? 'below-nav' : 'below-nav center-content'}>
               <div className='route-wrapper'>
                 <Routes>
-                  <Route path='dashboard' element={!user ? <Navigate to='/login' /> : <Dashboard />} />
-                  <Route path='login' element={user ? <Navigate to='/dashboard' /> : <Login />} />
-                  <Route path='signup' element={user ? <Navigate to='/dashboard' /> : <Signup />} />
+                  <Route path={'dashboard/:pageId'} element={!user ? <Navigate to='/login' /> : <Dashboard />} />
+                  <Route
+                    exact
+                    path='login'
+                    element={user ? <Navigate to={`/dashboard/${'page-' + currentPage}`} /> : <Login />}
+                  />
+                  <Route
+                    path='signup'
+                    element={user ? <Navigate to={`/dashboard/${'page-' + currentPage}`} /> : <Signup />}
+                  />
                   <Route path='projects/:id' element={<Project />} />
                 </Routes>
               </div>
