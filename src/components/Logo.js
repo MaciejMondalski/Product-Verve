202dac356c659b3138df562063006b89c1637e6c
MaciejMondalski <maciej.mondalski@gmail.com>
2022-08-10 21:45:07 +0200

    fixing logo colors when logged out

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import LogoImage from '../assets/pv_logo.svg';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Logo() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <StyledLogo>
       <Link to={'initiatives/page-1'}>
         <img src={LogoImage} alt='verve logo' />
-        <span>Product Verve</span>
+        <span className={`${!user && 'logo-purple'}`}>Product Verve</span>
       </Link>
     </StyledLogo>
   );
 }
 
 const StyledLogo = styled.div`
-  //padding: 20px 30px;
   height: 4.2em;
   box-sizing: border-box;
   display: flex;
@@ -30,10 +32,14 @@ const StyledLogo = styled.div`
   }
 
   span {
-    color: var(--bg-color);
-    font-size: 1.6em;
+    color: #fff;
+    font-size: 1.8em;
     font-family: 'Chakra Petch', sans-serif;
     font-weight: 700;
+  }
+
+  .logo-purple {
+    color: var(--primary-color);
   }
 
   img {
@@ -41,6 +47,9 @@ const StyledLogo = styled.div`
     filter: invert(95%);
     width: 32px;
     margin-top: -2px;
+  }
+
+  .logo-purple {
   }
 `;
 
