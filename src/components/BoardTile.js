6e02ec07ec4858caea0821e3bf94083ce6ea29e2
MaciejMondalski <maciej.mondalski@gmail.com>
2022-08-20 22:01:38 +0200

    board tile layout and styling

--- src/components/BoardTile.js
+++ src/components/BoardTile.js
@@ -1,13 +1,44 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 import styled from 'styled-components';
+import Avatar from './Avatar';
 
 const BoardTile = ({ project }) => {
+  const [shownName, setShowName] = useState(null);
+
   return (
     <StyledBoardTile>
-      <Link to={`/projects/${project.id}`} key={project.id}>
+      <Link to={`/project/${project.id}`} key={project.id}>
         <div className='top-part'>
           <h4>{project.name}</h4>
           <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className='bottom-part'>
+            <p
+              className={`priority ${
+                project.priority === 'High'
+                  ? 'high'
+                  : project.priority === 'Medium'
+                  ? 'medium'
+                  : project.priority === 'Low' && 'low'
+              }`}
+            >
+              {project.priority}
+            </p>
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li
+                  className={`${shownName === user && 'is-hovered'}`}
+                  onMouseEnter={() => setShowName(user)}
+                  onMouseLeave={() => setShowName(null)}
+                  key={user.photoURL}
+                >
+                  <Avatar src={user.photoURL} />
+                  {shownName === user && <p className='hover-name'>{user.displayName}</p>}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </Link>
     </StyledBoardTile>
@@ -18,7 +49,6 @@ const StyledBoardTile = styled.div`
   padding: 1em;
   margin-bottom: 8px;
   width: 100%;
-  height: 50px;
   background: #fff;
   border-radius: 0.5em;
   border: 1px solid var(--nice-gray);
@@ -26,11 +56,51 @@ const StyledBoardTile = styled.div`
   transition: transform 0.13s;
 
   .top-part {
-    border-bottom: 1px solid #eee;
-    padding-bottom: 10px;
-
     h4 {
       margin-bottom: 0.4em;
+    }
+
+    .priority {
+      color: black;
+      margin-top: 0;
+    }
+  }
+
+  .bottom-part {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 0.4em;
+
+    ul {
+      display: flex;
+    }
+    li {
+      margin-left: -0.9em;
+      position: relative;
+      transition: 0.2s;
+    }
+    .avatar {
+      width: 2.2em;
+      height: 2.2em;
+    }
+
+    .hover-name {
+      position: absolute;
+      background: grey;
+      color: #fff;
+      padding: 0.2em 0.4em;
+      border-radius: 0.5em;
+      top: 120%;
+      left: 50%;
+      transform: translate(-50%);
+      text-align: center;
+      white-space: nowrap;
+    }
+
+    .is-hovered {
+      z-index: 5;
+      transform: scale(1.05);
     }
   }
 
