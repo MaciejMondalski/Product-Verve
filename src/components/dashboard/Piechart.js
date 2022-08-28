37514eaa5a581a08a1c9ac6ed5df0d16fc8c5d6a
MaciejMondalski <maciej.mondalski@gmail.com>
2022-08-28 23:07:15 +0200

    added permanent precentage labels

--- src/components/dashboard/Piechart.js
+++ src/components/dashboard/Piechart.js
@@ -32,6 +32,19 @@ const Piechart = () => {
     setStatusList(chartList);
   }, [statuses, projects]);
 
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text className='label-text' x={x} y={y} fill='white' textAnchor='middle' dominantBaseline='central'>
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   const renderActiveShape = (props) => {
     const RADIAN = Math.PI / 180;
     const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
@@ -67,14 +80,18 @@ const Piechart = () => {
           innerRadius={outerRadius + 6}
           outerRadius={outerRadius + 10}
           fill={fill}
-        />{' '}
+        />
         <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill='none' />
         <circle cx={ex} cy={ey} r={2} fill={fill} stroke='none' />
-        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill='#333'>
+        <text
+          className='label-text'
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          dy={5}
+          textAnchor={textAnchor}
+          fill='#333'
+        >
           {`${value} ${value === 1 ? 'issue' : 'issues'}`}
-        </text>
-        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill='#999'>
-          {`(${(percent * 100).toFixed(2)}%)`}
         </text>
       </g>
     );
@@ -82,76 +99,96 @@ const Piechart = () => {
 
   return (
     <StyledPiechart>
-      <PieChart width={400} height={400}>
-        <Pie
-          activeIndex={activeIndex}
-          activeShape={renderActiveShape}
-          data={statusList}
-          cx={200}
-          cy={200}
-          innerRadius={60}
-          outerRadius={110}
-          fill='#8884d8'
-          dataKey='quantity'
-          onMouseEnter={onPieEnter}
-        >
-          {statusList &&
-            statusList.map((i, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
-        </Pie>
-      </PieChart>
-      <div className='chart-legend'>
-        {statuses &&
-          statuses.map((status, index) => {
-            return (
-              <div key={index} className='legend-item'>
-                <span style={{ background: COLORS[index] }}></span>
-                <p>{status.name}</p>
-              </div>
-            );
-          })}
+      <h2>Statuses</h2>
+      <div className='chart-wrapper'>
+        <PieChart width={400} height={400}>
+          <Pie
+            activeIndex={activeIndex}
+            activeShape={renderActiveShape}
+            label={renderCustomizedLabel}
+            labelLine={false}
+            data={statusList}
+            cx={200}
+            cy={200}
+            innerRadius={60}
+            outerRadius={110}
+            fill='#8884d8'
+            dataKey='quantity'
+            onMouseEnter={onPieEnter}
+          >
+            {statusList &&
+              statusList.map((i, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
+          </Pie>
+        </PieChart>
+        <div className='chart-legend'>
+          {statuses &&
+            statuses.map((status, index) => {
+              return (
+                <div key={index} className='legend-item'>
+                  <span style={{ background: COLORS[index] }}></span>
+                  <p>{status.name}</p>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </StyledPiechart>
   );
 };
 
 const StyledPiechart = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: white;
-  border-radius: 0.6em;
-  padding: 0 4em;
+  margin: 1.2em 1.4em;
 
-  svg {
-    overflow: inherit;
-    padding: 0 0px;
-    margin-top: -40px;
+  h2 {
+    display: flex;
+    color: var(--heading-color);
+    padding: 0.4em 0.6em;
   }
 
-  .center-text {
-    font-weight: 600;
-    font-size: 1.2em;
-  }
-
-  .chart-legend {
+  .chart-wrapper {
     display: flex;
-    justify-content: center;
-    margin-top: -70px;
-    margin-bottom: 30px;
-    z-index: 44;
-    display: flex;
-  }
-
-  .legend-item {
-    display: flex;
+    flex-direction: column;
     align-items: center;
-    margin: 0 0.6em;
+    background: white;
+    border-radius: 0.6em;
+    padding: 0 4em;
 
-    span {
-      width: 1em;
-      height: 1em;
-      border-radius: 0.2em;
-      margin-right: 0.2em;
+    svg {
+      overflow: inherit;
+      padding: 0 0px;
+      margin-top: -50px;
+    }
+
+    .center-text {
+      font-weight: 600;
+      font-size: 1.2em;
+    }
+
+    .label-text {
+      font-size: 1.1em;
+      font-weight: 500;
+    }
+
+    .chart-legend {
+      display: flex;
+      justify-content: center;
+      margin-top: -60px;
+      margin-bottom: 2em;
+      z-index: 1;
+      display: flex;
+    }
+
+    .legend-item {
+      display: flex;
+      align-items: center;
+      margin: 0 0.6em;
+
+      span {
+        width: 1em;
+        height: 1em;
+        border-radius: 0.2em;
+        margin-right: 0.2em;
+      }
     }
   }
 `;
