202adcde028fdb28732325f8c938d668b44750b3
MaciejMondalski <maciej.mondalski@gmail.com>
2022-08-28 23:05:02 +0200

    fixed firestore query and orderBy requests

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef } from 'react';
 import { db } from '../firebase/config';
 
 // firebase imports
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot, query, where, orderBy } from 'firebase/firestore';
 
 export const useCollection = (c, _query, _orderBy) => {
   const [documents, setDocuments] = useState(null);
@@ -12,17 +12,24 @@ export const useCollection = (c, _query, _orderBy) => {
   // Without a ref --> infinite loop in useEffect
   // _query is an array and is 'diffrent' on every function call
 
-  const query = useRef(_query).current;
-  const orderBy = useRef(_orderBy).current;
+  const propsQuery = useRef(_query).current;
+  const propsOrderBy = useRef(_orderBy).current;
+
+  // .collection("projects")
+  // .where("status", "==", "Done")
+
+  // `${queryRef ? queryRef : ref}`
 
   useEffect(() => {
     let ref = collection(db, c);
 
-    if (query) {
-      ref = ref.where(...query);
+    //  const q = query(ref, where(_query));
+    if (propsQuery) {
+      ref = query(ref, where(...propsQuery));
     }
-    if (orderBy) {
-      ref = ref.orderBy(...orderBy);
+
+    if (propsOrderBy) {
+      ref = query(ref, orderBy(...propsOrderBy));
     }
 
     const unsubscribe = onSnapshot(
@@ -45,7 +52,7 @@ export const useCollection = (c, _query, _orderBy) => {
 
     // unsubscribe on unmount
     return () => unsubscribe();
-  }, [c, query, orderBy]);
+  }, [c]);
 
-  return { documents, error };
+  return { documents, error, _query };
 };
