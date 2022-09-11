e79e6da17590ce424e60c58204fd213b60d6a93b
MaciejMondalski <maciej.mondalski@gmail.com>
2022-09-11 20:43:16 +0200

    created new component

---
+++ src/components/dashboard/CategoriesChart.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { useCollection } from '../../hooks/useCollection';
+import styled from 'styled-components';
+
+const CategoriesChart = () => {
+  const { documents: projects } = useCollection('projects');
+  const { documents: categories } = useCollection('categories');
+
+  const [categoryList, setCategoryList] = useState();
+
+  const COLORS = ['#C9BBFA', '#8677ED', '#4F42C7', '#322F7D'];
+
+  useEffect(() => {
+    const chartList =
+      categories &&
+      projects &&
+      categories.map((category) => {
+        const categoryQuantity = projects.filter((project) => project.category === category.value).length;
+        return { name: category.value, quantity: categoryQuantity };
+      });
+    setCategoryList(chartList);
+  }, [categories, projects]);
+
+  return (
+    <StyledCategories>
+      <div className='container'>
+        <h2>Categories</h2>
+        <ResponsiveContainer className='chart-wrapper' width='90%' height='80%'>
+          <BarChart
+            width={550}
+            height={300}
+            data={categoryList}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='name' />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey='quantity'>
+              {categoryList &&
+                categoryList.map((i, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </StyledCategories>
+  );
+};
+
+const StyledCategories = styled.div`
+  height: 100%;
+  width: 100%;
+
+  .container {
+    box-sizing: border-box;
+    height: 100%;
+  }
+
+  h2 {
+    display: flex;
+    color: var(--heading-color);
+    margin: 1.2em 0 0 1.4em;
+  }
+
+  .chart-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    margin: auto 0;
+  }
+`;
+
+export default CategoriesChart;
