88664bcdf023361776ba03587772acbd086b9058
MaciejMondalski <maciej.mondalski@gmail.com>
2022-10-23 23:53:54 +0200

    added a reducer

--- src/context/ProjectContext.js
+++ src/context/ProjectContext.js
@@ -1,20 +1,56 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useReducer, useState } from 'react';
 import { useCollection } from '../hooks/useCollection';
 
 export const ProjectContext = createContext();
 
+export const selectorReducer = (state, action) => {
+  switch (action.type) {
+    case 'SELECT':
+      return { ...state, project: action.payload };
+    default:
+      return state;
+  }
+};
+
 export const ProjectContextProvider = ({ children }) => {
-  const { documents, error } = useCollection('projectGroups');
   const [currentProject, setCurrentProject] = useState();
+  const [urlCurrentProject, setUrlCurrentProject] = useState();
+  const { documents } = useCollection('projectGroups');
+  const [state, dispatch] = useReducer(selectorReducer, {
+    project: null,
+  });
 
   useEffect(() => {
-    if (documents) {
-      const projectName = documents[0].projectName;
-      setCurrentProject(projectName);
-    }
-  }, [documents]);
+    console.log(state);
+  }, [state]);
 
-  console.log('ProjectContext state:', currentProject);
+  console.log('SelectorContext state:', state);
 
-  return <ProjectContext.Provider value={{ currentProject, setCurrentProject }}>{children}</ProjectContext.Provider>;
+  return <ProjectContext.Provider value={{ ...state, dispatch }}>{children}</ProjectContext.Provider>;
 };
+
+//  const [currentProject, setCurrentProject] = useState();
+//  const [urlCurrentProject, setUrlCurrentProject] = useState();
+//  const [projectObject, setProjectObject] = useState();
+
+//  useEffect(() => {
+//    if (documents) {
+//      const projectObj = documents[0];
+//      const projectName = documents[0].projectName;
+//      const urlProjectName = projectName.replace(/\s/g, '');
+//      setCurrentProject(projectName);
+//      setUrlCurrentProject(urlProjectName);
+//      setProjectObject(projectObj);
+//      console.log(projectObj);
+//    }
+//  }, [documents]);
+
+//  return (
+//    <ProjectContext.Provider
+//      value={{ currentProject, setCurrentProject, urlCurrentProject, setUrlCurrentProject, projectObject }}
+//    >
+//      {children}
+//    </ProjectContext.Provider>
+//  );
+//};
+//
