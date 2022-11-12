d12dbbde71ebfb2227f837fa0fd07538cf8c9a06
MaciejMondalski <maciej.mondalski@gmail.com>
2022-11-12 23:56:09 +0100

    .

--- src/context/ProjectContext.js
+++ src/context/ProjectContext.js
@@ -1,24 +1,35 @@
 import { createContext, useEffect, useReducer, useState } from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
 import { useCollection } from '../hooks/useCollection';
 
 export const ProjectContext = createContext();
 
 export const ProjectContextProvider = ({ children }) => {
   const { documents } = useCollection('projectGroups');
+  const { documents: users } = useCollection('users');
+  const { user } = useAuthContext();
 
   const [projectObject, setProjectObject] = useState();
   const [urlCurrentProject, setUrlCurrentProject] = useState();
 
   useEffect(() => {
-    const selectedProject = documents && documents.filter((project) => project.selected == true)[0];
-    console.log(selectedProject);
+    const pickUser = async () => {
+      const userSelection = user && users && users.filter((currentUser) => currentUser.id == user.uid);
 
-    setProjectObject(selectedProject);
-  }, [documents]);
+      const userObject = (await userSelection) && userSelection.map((user) => user)[0];
+
+      const selectedProject =
+        userObject && documents && documents.filter((project) => project.id == userObject.selectedProjectId)[0];
+
+      setProjectObject(selectedProject);
+    };
+
+    pickUser();
+  }, [documents, users]);
 
   useEffect(() => {
     projectObject && setUrlCurrentProject(projectObject.projectName.replace(/\s/g, ''));
-    console.log(urlCurrentProject);
+    console.log(projectObject);
   }, [projectObject]);
 
   return (
